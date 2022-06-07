b4c1bcaeead1410a3176fa75734527b938905087
zhanghouyi <zhanghouyi@baoxiaohe.com>
2022-06-07 16:58:22 +0800

    提交ai插件electron

---
+++ main.js
@@ -0,0 +1,84 @@
+/*
+ * @Author: zhanghouyi zhanghouyi@baoxiaohe.com
+ * @Date: 2022-06-06 14:05:15
+ * @LastEditors: zhanghouyi zhanghouyi@baoxiaohe.com
+ * @LastEditTime: 2022-06-07 14:58:14
+ * @FilePath: /electron-app/electronjs/main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const { app, BrowserWindow } = require("electron");
+const path = require('path');
+const fs=require('fs');
+
+let win;
+const appName = '../vue-app';
+
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    // and load the index.html of the app.
+    win.loadFile("index.html");
+    win.webContents.openDevTools();
+    // Open the DevTools.
+    // win.webContents.openDevTools();
+    win.webContents.on('did-finish-load', installPlugin);
+    win.on('closed', function () {
+        win = null
+    })
+}
+function installPlugin () {
+    let baseBath
+    let installPath
+  
+    if (process.platform === 'darwin') {
+      // 测试不能加载插件
+      // baseBath = process.env['HOME'] + '/Library/Application Support/Adobe/'
+      baseBath = '/Library/Application Support/Adobe/'
+    } else {
+      baseBath = process.env['USERPROFILE'] + '/AppData/Roaming/Adobe/'
+    }
+  
+    installPath = baseBath + 'CEP/extensions/'
+    movePlugin(installPath)
+}
+  
+  function movePlugin (installPath) {
+    fs.writeFileSync(path.join(__dirname, 'drag-and-drop-1.md'), '# First file to test drag and drop')
+    // fs.chmod(path.resolve(installPath),0o400,function(err){
+    //   console.log('err',err)
+    //   console.log('aaaaaa',path.resolve(installPath));
+    //   fs.writeFileSync('example.txt', "This file has now been edited.");
+    // })
+    // fs.emptyDir(installPath, function (mkdirErr) {
+    //     // path exists unless there was an error
+    //     console.error('------',mkdirErr)
+    //     fs.copy(path.join(__dirname, appName), path.resolve(installPath), function (err) {
+    //       let message = 'loaded'
+    //       if (err) {
+    //         console.error('011111',err)
+    //         message = err
+    //       }
+    //       win.webContents.send('status', message)
+    //     })
+    // })
+  }
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
