a0f6960582f2a3c922872790ce5d170e32a81f76
zhanghouyi <zhanghouyi@baoxiaohe.com>
2022-06-08 17:01:34 +0800

    提交代码

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * @Author: zhanghouyi zhanghouyi@baoxiaohe.com
  * @Date: 2022-06-06 14:05:15
  * @LastEditors: zhanghouyi zhanghouyi@baoxiaohe.com
- * @LastEditTime: 2022-06-08 16:46:59
+ * @LastEditTime: 2022-06-08 17:00:34
  * @FilePath: /electron-app/electronjs/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -126,7 +126,7 @@ function _copyDir(src, dist, callback) {
                     fs.writeFileSync(_dist, fs.readFileSync(_src));
                   } else if(stat.isDirectory()) {
                     // 当是目录是，递归复制
-                    copyDir(_src, _dist, callback)
+                    _copyDir(_src, _dist, callback)
                   }
                 }
               })
