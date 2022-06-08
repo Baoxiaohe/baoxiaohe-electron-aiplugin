4da75bfaa51c91d3c8ea9c52808e90f27a8e5c5b
zhanghouyi <zhanghouyi@baoxiaohe.com>
2022-06-08 16:24:40 +0800

    提交代码

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
  * @Author: zhanghouyi zhanghouyi@baoxiaohe.com
  * @Date: 2022-06-06 14:05:15
  * @LastEditors: zhanghouyi zhanghouyi@baoxiaohe.com
- * @LastEditTime: 2022-06-07 14:58:14
+ * @LastEditTime: 2022-06-08 16:23:10
  * @FilePath: /electron-app/electronjs/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const { app, BrowserWindow } = require("electron");
 const path = require('path');
-const fs=require('fs');
+const fs=require('fs-extra');
 
 let win;
-const appName = '../vue-app';
+const appName = '../zw';
 
 function createWindow() {
     // Create the browser window.
@@ -39,8 +39,8 @@ function installPlugin () {
   
     if (process.platform === 'darwin') {
       // 测试不能加载插件
-      // baseBath = process.env['HOME'] + '/Library/Application Support/Adobe/'
-      baseBath = '/Library/Application Support/Adobe/'
+      baseBath = process.env['HOME'] + '/Library/Application Support/Adobe/'
+    //   baseBath = '/Library/Application Support/Adobe/'
     } else {
       baseBath = process.env['USERPROFILE'] + '/AppData/Roaming/Adobe/'
     }
@@ -59,7 +59,7 @@ function installPlugin () {
     let home = process.env['HOME']
     
     // 打dmg文件，__dirname 对应的是/Volumes/ai-plugin 1.0.0/ai-plugins.app/Contents/Resources/app.asar。
-    fs.copyFileSync(path.join(__dirname,'../zw/hello.html'), path.join(home,'/Library/Application Support/Adobe/CEP/extensions/hello.html'))
+    // fs.copyFileSync(path.join(__dirname,'../zw'), path.join(home,'/Library/Application Support/Adobe/CEP/extensions/'))
     
     // fs.chmod(path.resolve(installPath),0o400,function(err){
     //   console.log('err',err)
@@ -67,18 +67,18 @@ function installPlugin () {
     //   fs.writeFileSync('example.txt', "This file has now been edited.");
     // })
 
-    // fs.emptyDir(installPath, function (mkdirErr) {
-    //     // path exists unless there was an error
-    //     console.error('------',mkdirErr)
-    //     fs.copy(path.join(__dirname, appName), path.resolve(installPath), function (err) {
-    //       let message = 'loaded'
-    //       if (err) {
-    //         console.error('011111',err)
-    //         message = err
-    //       }
-    //       win.webContents.send('status', message)
-    //     })
-    // })
+    fs.emptyDir(installPath, function (mkdirErr) {
+        // path exists unless there was an error
+        console.error('------',mkdirErr)
+        fs.copy(path.join(__dirname, appName), path.resolve(installPath), function (err) {
+          let message = 'loaded'
+          if (err) {
+            console.error('011111',err)
+            message = err
+          }
+          win.webContents.send('status', message)
+        })
+    })
   }
 app.whenReady().then(createWindow);
 
